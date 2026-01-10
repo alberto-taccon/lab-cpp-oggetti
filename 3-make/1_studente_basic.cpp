fa81bbf190388887e6973eef8eb12d28123501a9
alberto-taccon <97546990+alberto-taccon@users.noreply.github.com>
2026-01-10 11:50:39 +0100

    Update 1_studente_basic.cpp

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -22,10 +22,22 @@ int main()
     /*********************** Creazione di oggetti ******************************/
     // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
     // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
+    
+    Studente mario;
+    mario.nome="Mario";
+    mario.eta=17;
+    mario.media=7;
 
-
+    Studente maria;
+    Studente* p = &maria;
+    p->nome="Maria";
+    p->eta=19;
+    p->media=8;
+    
     /************************** Visualizzazione ********************************/
     // TODO Visualizzare il nome e la media sia di Mario che Maria
 
+    mario.stampaStudente();
+    p->stampaStudente();
     return 0;
 }
