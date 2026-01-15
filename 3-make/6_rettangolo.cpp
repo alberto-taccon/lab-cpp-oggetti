3509e36a7a9c7b98524b7f4f754ca1c8d2ac00af
alberto-taccon <97546990+alberto-taccon@users.noreply.github.com>
2026-01-15 09:44:35 +0100

    Update 6_rettangolo.cpp

--- 3-make/6_rettangolo.cpp
+++ 3-make/6_rettangolo.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
  /********************************************************************************************
- *                                  Implementazione classe Rettangolo                        *     
+ *                                  Implementazione classe Rettangolo                        *
  *                                                                                           *
  *   Implementare una classe Rettangolo, che modelli la figura geometrica                    *
  *   Implementarla in maniera coerente ai seguenti requisiti:                                *
@@ -13,19 +13,49 @@ using namespace std;
  *********************************************************************************************/
 
 class Rettangolo {
-
+    private:
     // Campi
-
-    // Costruttore
-
+        float base;
+        float altezza;
+    public:
     // Metodi set... e get...
-
+        void setBase(float nuova_base){
+            base = nuova_base;
+        }
+        void setAltezza(float nuova_altezza){
+            altezza = nuova_altezza;
+        }
+        float getBase(){
+            return base;
+        }
+        float getAltezza(){
+            return altezza;
+        }
     // Altri metodi
+        float calcolaPerimetro(){
+            return (base+altezza)*2;
+        }
+        float calcolaArea(){
+            return base*altezza;
+        }
 };
 
 int main()
 {
-    // TODO testare
+    Rettangolo r;
+    float n_base, n_altezza;
 
+    // TODO testare
+    cout << "Inserisci base: ";
+    cin >> n_base;
+    r.setBase(n_base);
+    cout << "Inserisci altezza: ";
+    cin >> n_altezza;
+    r.setAltezza(n_altezza);
+
+    cout << "La base e': " << r.getBase() << endl;
+    cout << "La altezza e': " << r.getAltezza() << endl;
+    cout << "Il perimetro e': " << r.calcolaPerimetro() << endl;
+    cout << "L'area e': " << r.calcolaArea() << endl;
     return 0;
 }
