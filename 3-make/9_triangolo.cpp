98260bddf7914eedca189fd52be6c735e4c59142
alberto-taccon <alberto.taccon@icloud.com>
2026-01-15 21:20:13 +0100

    Updated 9_triangolo.cpp

--- 3-make/9_triangolo.cpp
+++ 3-make/9_triangolo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -8,21 +9,53 @@ class triangolo{
         float lato3;
         float lato2;
     public:
-        float setLato1(float nuovo_lato1){
+        void setLato1(float nuovo_lato1){
             lato1=nuovo_lato1;
         }
-        float setLato2(float nuovo_lato2){
+        void setLato2(float nuovo_lato2){
             lato2=nuovo_lato2;
         }
-        float setLato3(float nuovo_lato3){
+        void setLato3(float nuovo_lato3){
             lato3=nuovo_lato3;
         }
-        //isoscele,
+        bool isIsoscele(){
+            if(lato1==lato2 || lato1==lato3 || lato2==lato3){
+                return true;
+            }else{
+                return false;
+            }
+        }
         float calcolaPerimetro(){
             float p = (lato1 + lato2 + lato3);
             return p;
         }
-        //float area
+        float calcolaAltezza(){
+            float base, altezza;
+            if(lato1==lato2){
+                base = lato3;
+                altezza = sqrt((lato1*lato1) - ((base*base)/4));
+            }else if(lato1==lato3){
+                base = lato2;
+                altezza = sqrt((lato1*lato1) - ((base*base)/4));
+            }else{
+                base = lato1;
+                altezza = sqrt((lato2*lato2) - ((base*base)/4));
+            }
+            return altezza;
+        }
+        float calcolaArea(){
+            float base, area;
+            if(lato1==lato2){
+                base = lato3;
+            }else if(lato1==lato3){
+                base = lato2;
+            }else{
+                base = lato1;
+            }
+            area = (base * calcolaAltezza()) / 2;
+            return area;
+        }
+
 };
 
 int main()
@@ -40,6 +73,11 @@ int main()
     cin >> lato3;
     t.setLato3(lato3);
     cout << "Perimetro: " << t.calcolaPerimetro();
-     
+    cout << "\nArea: " << t.calcolaArea();
+    if(t.isIsoscele()){
+        cout << "\nIl triangolo e' isoscele";;
+    }else{
+        cout << "\nIl triangolo non e' isoscele";;
+    }
     return 0;
 }
